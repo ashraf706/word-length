e95cad1e20dc23bfa6f40761661a244bfe355503
ashraful.hasan <ahasan@unrulygroup.com>
2020-04-29 20:07:10 +0100

    Feature: Update App to make it more presentable

--- src/main/java/com/candidate/App.java
+++ src/main/java/com/candidate/App.java
@@ -1,8 +1,16 @@
 package com.candidate;
 
+import java.util.Map;
+
 public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String str = "The cow jumped over the moon.";
+
+        WordLength wordLength = new WordLength();
+        Map<String, Object> result = wordLength.getLongestWordAndLength(str);
+
+        System.out.println( "Longest word: " + result.get("word") );
+        System.out.println( "Word length: " + result.get("length") );
     }
 }
